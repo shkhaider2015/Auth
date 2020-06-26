65d3240a1d60c7564563000ac9cadb3c2615f69f
Haider <shkhaider2015@gmail.com>
2020-06-27 03:20:07 +0500

    asd

--- application/models.py
+++ application/models.py
@@ -4,5 +4,5 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=False, nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
-    image_file = db.Column(db.String(100), nullable=False, default='default.jpg')
+    image_file = db.Column(db.String(100), nullable=True, default='default.jpg')
     password = db.Column(db.String(20), nullable=False)
--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,6 @@
 from flask import jsonify, request
-from application import app
+from application import app, db
+from application.models import User
 
 
 @app.route('/')
@@ -14,5 +15,15 @@ def add_user():
     password = request.json['password']
     # image_file = request.files['image_file']
     print(f" Name : {name} \n Email : {email} \n Password : {password} ")
+    user = User(name=name, email=email, password=password)
+    db.session.add(user)
+    db.session.commit()
 
     return jsonify({ 'name' : name, 'emai' : email, 'password' : password })
+
+@app.route('/checkemail/<string:email>', methods=['GET'])
+def checkAvailablity(email):
+    email = email
+    user = User.query.filter_by(email=email).first()
+    print(user.name)
+    return jsonify({ 'name' : user.name, 'email' : user.email, 'password' : user.password})
